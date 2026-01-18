b01e936e2b3efc9873b53fe3579c5c9ada68c100
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-01-18 21:59:58 +0000

    add cli entrypoint

--- cmd/efrc/main.go
+++ cmd/efrc/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "efrc",
+	Short: "Personal mesh VPN client",
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
