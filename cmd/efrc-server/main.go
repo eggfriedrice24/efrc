9f957e4731a17228b0dd2595e623da4107d6f90c
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-01-18 20:00:07 +0000

    add cobra, migrate server flag to cobra

--- cmd/efrc-server/main.go
+++ cmd/efrc-server/main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"efrc/internal/server"
+
+	"github.com/spf13/cobra"
 )
 
+var (
+	port       int
+	networkKey string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "efrc-server",
+	Short: "Control server for efrc mesh VPN",
+	Run: func(cmd *cobra.Command, args []string) {
+		if networkKey == "" {
+			log.Fatal("--network-key is required")
+		}
+
+		store := server.NewStore()
+		api := server.NewAPI(store, networkKey)
+		handler := api.SetupRoutes()
+
+		addr := fmt.Sprintf(":%d", port)
+		fmt.Printf("Server starting on %s\n", addr)
+		log.Fatal(http.ListenAndServe(addr, handler))
+	},
+}
+
+func init() {
+	rootCmd.Flags().IntVarP(&port, "port", "p", 2424, "Server port")
+	rootCmd.Flags().StringVarP(&networkKey, "network-key", "k", "", "Networkauthentication key")
+	rootCmd.MarkFlagRequired("network-key")
+}
+
 func main() {
-	port := flag.Int("port", 2424, "Server port")
-	networkKey := flag.String("network-key", "", "Network authentication key")
-	flag.Parse()
-
-	if *networkKey == "" {
-		log.Fatal("--network-key is required")
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
 	}
-
-	store := server.NewStore()
-	api := server.NewAPI(store, *networkKey)
-	handler := api.SetupRoutes()
-
-	addr := fmt.Sprintf(":%d", *port)
-	fmt.Printf("Server starting on %s\n", addr)
-	log.Fatal(http.ListenAndServe(addr, handler))
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,9 @@ require (
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20241231184526-a9ab2273dd10
 )
 
-require golang.org/x/crypto v0.31.0 // indirect
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.10.2 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+)
