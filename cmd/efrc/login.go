0cbfd18b39e8b224fa6bd92756795c5837bcc3a3
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-01-18 22:00:25 +0000

    add login command

---
+++ cmd/efrc/login.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"efrc/internal/client"
+
+	"github.com/spf13/cobra"
+)
+
+var serverURL string
+
+var loginCmd = &cobra.Command{
+	Use:   "login [network-key]",
+	Short: "Configure server and network key",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		config, err := client.LoadConfig()
+		if err != nil {
+			log.Fatalf("No config found. Run 'efrc init' first: %v", err)
+		}
+
+		config.NetworkKey = args[0]
+		config.ServerURL = serverURL
+
+		if err := config.Save(); err != nil {
+			log.Fatalf("Failed to save config: %v", err)
+		}
+
+		fmt.Println("Login configured!")
+		fmt.Printf("Server: %s \n", config.ServerURL)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(loginCmd)
+	loginCmd.Flags().StringVarP(&serverURL, "server", "s", "", "Server URL")
+	loginCmd.MarkFlagRequired("server")
+}
